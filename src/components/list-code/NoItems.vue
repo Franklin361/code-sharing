2a4a25e65d333d3c79c2b16cd647f09eed8f9304
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2024-04-09 12:12:31 -0600

    style: :lipstick: update NoItems.vue

--- src/components/list-code/NoItems.vue
+++ src/components/list-code/NoItems.vue
@@ -1,5 +1,27 @@
+<script setup lang="ts">
+import Button from '@/components/ui/button/Button.vue';
+import { RouteNames } from '@/router/main';
+</script>
+
 <template>
-  <div class="opacity-50 text-4xl font-bold my-16 text-center">
-    No codes to share 📝
+  <div class="flex justify-center items-center flex-col gap-2 min-h-[500px]">
+    <img src="https://cdn-icons-png.flaticon.com/512/10479/10479781.png" alt="icon"
+      class="w-[100px] object-cover -rotate-12" loading="eager">
+    <span class="opacity-50 text-4xl font-bold text-center">No codes to share 📝</span>
+    <router-link :to="{ name: RouteNames.EDITOR }" class="mt-5">
+      <Button variant="outline">Create a new code</Button>
+    </router-link>
   </div>
 </template>
+
+<style scoped>
+img {
+  animation: img-rotation .5s linear infinite alternate;
+}
+
+@keyframes img-rotation {
+  to {
+    transform: rotate(0deg);
+  }
+}
+</style>
