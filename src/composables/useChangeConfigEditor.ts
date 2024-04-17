9a4ea061c29183371a8dd27dd40008df78c1c1b4
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2024-04-17 16:52:55 -0600

    fix: :green_heart: remove useChangeConfigEditor.ts

--- src/composables/useChangeConfigEditor.ts
+++
@@ -1,8 +0,0 @@
-import { getLanguage, getTheme } from '@/lib/utils';
-import { useCodeStore } from '@/store/mainStore';
-import { Code } from '@/types/code';
-import { ref, shallowRef, watch } from 'vue';
-
-export function useChangeConfigEditor(defaultValues?: Code) {
-  // return { language, description, theme, code, updateValue };
-}
